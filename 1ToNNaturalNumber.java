52757d090fe04ee7671602e8b52adb09bd0b6be6
Sakshi Chaurasiya <56647481+sakshichaurasiya123@users.noreply.github.com>
2020-12-09 16:26:33 +0530

    Create 1ToNNaturalNumber.java

---
+++ 1ToNNaturalNumber.java
@@ -0,0 +1,20 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int n=in.nextInt();
+	    print(n);
+	}
+	static void print(int n){
+	    if(n==1)
+	   {
+	       System.out.println(n);
+	       return;
+	   }
+	   print(n-1);
+	   System.out.println(n);
+	}
+}
